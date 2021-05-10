6219bf68dd6020cc1195cf15bfc8ffa459cba19f
German G Sayago <german.sayago@gmail.com>
2021-05-10 16:46:49 -0300

    feature: hero listo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import BtnScroll from './BtnScroll';
+import HeroImage from '../assets/static/hero-bg-2.webp';
 
 export default function Hero() {
   return (
     <section className='hero'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-6'>texto</div>
-          <div className='col-md-6'>foto</div>
+      <img src={HeroImage} alt='' className='hero-bg' />
+      <div className='hero-content'>
+        <div className='container'>
+          <div className='row justify-content-center'>
+            <div className='col-md-8'>
+              <p className='hero-antetitle'>Villa Carlos Paz</p>
+              <h1 className='hero-title'>
+                Tu departamento en las sierras de Córdoba
+              </h1>
+              <h3 className='hero-subtitle'>
+                ¡El momento de invertir es ahora!
+              </h3>
+              {/* <ul>
+                <li>Entrega Mínima</li>
+                <li>
+                  <span>Plan Cosecha:</span> 10 pagos semestrales, 10% de
+                  entrega
+                </li>
+                <li>
+                  Finaciación hasta en <span>60 cuotas</span>
+                </li>
+              </ul> */}
+              <div>
+                <BtnScroll textBtn='Consultanos' classBtn='btn-cta' />
+              </div>
+            </div>
+          </div>
         </div>
-        <BtnScroll textBtn='Consultanos' classBtn='btn-primary' />
       </div>
     </section>
   );
--- src/template/Navbar.js
+++ src/template/Navbar.js
@@ -4,7 +4,7 @@ import LogoCustomer from '../assets/static/logo-veneto.svg';
 
 export default function Navbar() {
   return (
-    <nav className='navbar navbar-light bg-light'>
+    <nav className='navbar navbar-light bg-light sticky-top'>
       <div className='container'>
         <Link className='navbar-brand mx-auto' to='/'>
           <img src={LogoCustomer} alt='' />
